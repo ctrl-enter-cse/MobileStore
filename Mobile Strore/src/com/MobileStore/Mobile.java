c4219f99dbe0778e55ead0847a6bdf180496d155
munna <prasannacharaya@gmail.com>
2022-08-06 11:51:06 +0530

    adding

--- Mobile Strore/src/com/MobileStore/Mobile.java
+++ Mobile Strore/src/com/MobileStore/Mobile.java
@@ -2,45 +2,54 @@ package com.MobileStore;
 
 public class Mobile {
 
+
+	private String Model;
+	private String  name;
+	private int ram;
+	private int space;
+	private long price;
+	private int stock;
 	
+	
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String model) {
+		Model = model;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRam() {
+		return ram;
+	}
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	public int getSpace() {
+		return space;
+	}
+	public void setSpace(int space) {
+		this.space = space;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
 
-
-public String Name;
-public int ram;
-public int storage;
-public long price ;
-public int  stock;
-public String getName() {
-	return Name;
-}
-public void setName(String name) {
-	Name = name;
-}
-public int getRam() {
-	return ram;
-}
-public void setRam(int ram) {
-	this.ram = ram;
-}
-public int getStorage() {
-	return storage;
-}
-public void setStorage(int storage) {
-	this.storage = storage;
-}
-public long getPrice() {
-	return price;
-}
-public void setPrice(long price) {
-	this.price = price;
-}
-public int getStock() {
-	return stock;
-}
-public void setStock(int stock) {
-	this.stock = stock;
-}
-
+	
+	
 
 
 }
--- Mobile Strore/src/com/MobileStore/WelcomePage.java
+++ Mobile Strore/src/com/MobileStore/WelcomePage.java
@@ -1,14 +1,127 @@
 package com.MobileStore;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
 public class WelcomePage {
 
 	public static void main(String[] args) {
-				System.out.println("welcome to store");
-				System.out.println("1) press 1 to list all mobiles");
-				System.out.println("2) search a perticular model");
-				System.out.println("3) login as admin");
-
 				
+		
+		
+		
+	Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("-------------");
+		System.out.println("welcome to store ");
+//		1) press 1 to list all mobiles
+//		2) search a perticular model
+//		3) login as admin
+
+		
+		int num=3;
+		
+		if(num==1){
+			
+			
+			
+//			list of mobiles -> brands  then -> modells in that brands
+			
+			
+		}
+		if (num==2){
+		
+//			search by sepcific model   of  mobile 
+//			then back to step one buy process
+//			
+//			(here the spcific behaviour)
+//	
+			
+			searchMobileByModel(scanner);
+			
+			
+		}
+		if(num==3){
+//			then ask for admin  user id and pass 
+//			then give him access to add  the mobile into the list 
+			try {
+				System.out.println("welcome to addmin page ");
+				
+				addMobileToDb(scanner);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		
+		
 	}
+
+	private static void addMobileToDb(Scanner scanner) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("number of moble to add");
+		int num=scanner.nextInt();
+		ArrayList<Mobile> mobilelist= new  ArrayList<Mobile>();
+		
+		for(int i=0;i<num;i++){	
+			Mobile m=new Mobile(); 
+		System.out.println("enter the mobile model");
+		scanner.nextLine();
+		String Model=scanner.nextLine();
+		System.out.println("Model"+Model);
+		System.out.println("name of the mobile");
+//		scanner.nextLine();
+		String name=scanner.nextLine();
+		System.out.println("name:"+name);
+		
+		System.out.println("size of the ram ");
+//		int ram=scanner.nextInt();
+//		
+//		System.out.println("size of the Storage ");
+//		int space=scanner.nextInt();
+//		System.out.println("size of the price ");
+//		Long price=scanner.nextLong();
+//		System.out.println("size of the Stock ");
+//		int stock=scanner.nextInt();
+//		
+//		m.setModel(Model);
+//		m.setName(name);
+//		m.setRam(ram);
+//		m.setSpace(space);
+//		m.setPrice(price);
+//		m.setStock(stock);
+//		m.setStock(stock);
+//		m.setPrice(price);
+//		
+//		mobilelist.add(m);
+//		
+//		}
+//		
+//			SaveMobileListToDb(mobilelist);	
+	}}
+
+	private static void SaveMobileListToDb(ArrayList<Mobile> mobilelist) throws IOException {
+		ArrayList<Mobile>list =mobilelist;
+		FileWriter writer= new FileWriter("C:/Users/munna/Desktop/Mobile.csv");
+		for(int i=0;i<list.size();i++){
+			Mobile mob=list.get(i);
+		writer.write(mob.toString());
+		writer.flush();
+		
+		}
+		writer.close();
+	}
+		
+		
+		
+	
 
 }
